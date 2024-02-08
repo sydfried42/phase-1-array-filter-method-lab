febf9773a66487f9555260ee46dd6c0f2901a44c
sydneyfriedman <friedman.sydneyl@gmail.com>
2024-02-08 18:15:40 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, searchString) {
+    return drivers.filter(driver => {
+        return driver.toLowerCase() === searchString.toLowerCase()
+    }); 
+};
+
+function fuzzyMatch(drivers, initials) {
+    return drivers.filter(driver => {
+        return driver.startsWith(initials);
+    });
+};
+
+function matchName(drivers, something) {
+    return drivers.filter(driver => {
+        return driver.name === something
+    });
+};
